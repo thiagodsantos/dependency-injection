7644e62ff8da82058ff9609c47d8034dd0a7fe95
Thiago Santos <thiago.santos@outlook.com>
2021-08-10 20:45:27 -0300

    feat: adicionado cors

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import compression from "compression";
 import helmet from "helmet";
+import cors from "cors";
 import routes from "@src/infrastructure/rest/routes";
 
 export const app = express()
   .use(express.json())
   .use(compression())
   .use(helmet())
+  .use(cors({ origin: process.env.CORS ?? '*' }))
   .use(routes);
