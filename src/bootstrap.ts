5877e38b052a815e52506fb21275bcf3e018f948
Thiago Santos <thiago.santos@outlook.com>
2021-08-10 00:47:55 -0300

    feat: adicionar helmet e compression

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import compression from "compression";
+import helmet from "helmet";
 import routes from "@src/infrastructure/rest/routes";
 
 export const app = express()
   .use(express.json())
+  .use(compression())
+  .use(helmet())
   .use(routes);
